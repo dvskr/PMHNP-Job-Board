93a37c4c59a7199a5842ffd854de24878d4430db
Sathish Kumar <daggulasatish143@gmail.com>
2026-01-14 10:14:29 -0600

    latest

--- next.config.ts
+++ next.config.ts
@@ -41,15 +41,6 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png|webp|avif|ico)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
         source: '/_next/static/:path*',
         headers: [
           {
